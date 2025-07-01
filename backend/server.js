61f7c31595249bf8446680f3edb1ddd7fdee11c6
vishal2005025 <vishalmalyan2005@gmail.com>
2025-07-01 14:25:19 +0530

    cors origin set true

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, 
+    origin: true, // Allow all origins
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
